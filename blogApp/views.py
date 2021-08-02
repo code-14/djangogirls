d07c239007a7ce397be41c0da22c8b618f318858
code-14 <aboli1497@gmail.com>
2021-08-02 17:44:00 +0530

    Changed the HTML for the site.

--- blogApp/views.py
+++ blogApp/views.py
@@ -1,3 +1,13 @@
+'''
+ It will request information from the model you created before and pass it to a template
+ As you can see, we created a function (def) called post_list that takes request 
+ and will return the value it gets from calling another function render 
+ that will render (put together) our template blog/post_list.html
+
+'''
 from django.shortcuts import render
+
+def post_list(request):
+    return render(request, 'blogApp/post_list.html', {})
 
 # Create your views here.
